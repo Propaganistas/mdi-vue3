4dda8b570ec9773bfe37c808f77eb5edc96c8f6b
Propaganistas <Propaganistas@users.noreply.github.com>
2021-11-24 22:00:31 +0100

    push

--- scripts/version.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-fs.writeFileSync(__dirname + '/../package.json', JSON.stringify({
-	...require(__dirname + '/../package.json'),
-	version: require(__dirname + '/../node_modules/@mdi/svg/package.json').version
-}, null, 2))
