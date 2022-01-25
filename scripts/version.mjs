61b7c70ac1f7e8d0587239786ef700ee6c73933a
Propaganistas <Propaganistas@users.noreply.github.com>
2022-01-25 09:55:25 +0100

    Update version.mjs

--- scripts/version.mjs
+++ scripts/version.mjs
@@ -1,17 +1,22 @@
 import { readFileSync, writeFile } from 'fs'
+import { join } from 'path'
 import { fileURLToPath } from 'url'
 
 const rootDir = fileURLToPath(new URL('../', import.meta.url))
 
 //
+// Fetch package.json definition(s)
+//
+
+const mdi = JSON.parse(readFileSync(join(rootDir, 'node_modules/@mdi/js/package.json')))
+let pkg = JSON.parse(readFileSync(join(rootDir, 'package.json')))
+
+//
 // Update version in package.json
 //
 
-const mdi = JSON.parse(readFileSync(rootDir + '/node_modules/@mdi/js/package.json'))
-let pkg = JSON.parse(readFileSync(rootDir + '/package.json'))
-
 pkg.version = mdi.version
 
-writeFile(rootDir + '/package.json', JSON.stringify(pkg, null, 2), () => { 
+writeFile(join(rootDir, 'package.json'), JSON.stringify(pkg, null, 2), () => { 
     console.log(`Set package version to ${mdi.version}`) 
 })
