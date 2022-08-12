b97b7c6dd202aa770fa189064988db13a1b1072e
Propaganistas <Propaganistas@users.noreply.github.com>
2022-08-12 20:57:38 +0200

    Support title attribute

--- templates/icon.js
+++ templates/icon.js
@@ -3,6 +3,10 @@ import { h, defineComponent } from 'vue'
 export default defineComponent({
     name: <%= JSON.stringify(icon.name) %>,
 
+    props: {
+        title: String,
+    },
+
     render() {
         return h('svg', {
             xmlns: "http://www.w3.org/2000/svg",
@@ -10,6 +14,7 @@ export default defineComponent({
             viewBox: '0 0 24 24',
             'aria-hidden': true,
         }, [
+            this.title && this.title.length > 0 ? h('title', this.title) : null,
             h('path', {
                 d: <%= JSON.stringify(icon.path) %>,
             })
